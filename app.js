fd8701502ca4584c6d1d334d84f6c119ea465462
Gal Amrani <galamrani2@gmail.com>
2023-08-15 23:57:09 +0300

    Begin implementation of GET and POST, set up local host DB

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+// Connect to the MongoDB database
+mongoose.connect('mongodb://127.0.0.1:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on('error', console.error.bind(console, 'Connection error:'));
+mongoose.connection.once('open', () => {
+  console.log('Connected to Mongo postsDB');
+});
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model('User', userSchema);
+
+
+
+
 
 
 app.get("/", (req, res) => {
@@ -19,6 +45,35 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
+app.post("/register", (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+    saveUserToDB(email, password);
+    res.render('secrets');
+});
+
+app.post("/login", (req, res) => {
+
+});
+
+
+async function findUserInDB(email, password){
+
+}
+
+async function saveUserToDB(email, password) {
+    try{
+        const newUser = new User ({
+            email: email,
+            password: password
+        });
+        await newUser.save();
+        console.log("user saved to db");
+    }catch (err) {
+        console.error("Error saving user:", err);
+        throw err;
+    }
+}
 
 
 
